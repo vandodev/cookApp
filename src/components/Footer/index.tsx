3183af1c02bf838e06d8df7fabc418162720ae73
Evandro oliveira <vandoaparecido@hotmail.com>
2021-09-05 00:58:52 -0300

    :pizza: Animando footer com Moti

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,28 +1,37 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 
-import { styles } from './styles';
-import { Button } from '../Button';
+import { styles } from "./styles";
+import { Button } from "../Button";
+
+import { MotiView } from "moti";
 
 export function Footer() {
   return (
-    <View>
-      <Text style={styles.label}>
-        Detail
-      </Text>
+    <MotiView
+      from={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+      }}
+      transition={{
+        type: "timing",
+        duration: 3000,
+      }}
+    >
+      <Text style={styles.label}>Detail</Text>
 
       <Text style={styles.text}>
-        Get 50% discount on the special and delicious sushi
-        and stay connected for further discounts.
+        Get 50% discount on the special and delicious sushi and stay connected
+        for further discounts.
       </Text>
 
       <View style={styles.footer}>
-        <Text style={styles.price}>
-          $22
-        </Text>
+        <Text style={styles.price}>$22</Text>
 
         <Button />
       </View>
-    </View>
+    </MotiView>
   );
 }
