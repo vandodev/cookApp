77b43526bc3524b42ea27ce4343af16a39439351
Evandro oliveira <vandoaparecido@hotmail.com>
2021-09-05 00:47:18 -0300

    :pizza: Animando header com react-native-reanimated

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,38 @@
-import React from 'react';
-import { Feather } from '@expo/vector-icons';
-import { View, Text } from 'react-native';
+import React, { useEffect } from "react";
+import { Feather } from "@expo/vector-icons";
+import { View, Text } from "react-native";
 
-import { styles } from './styles';
-import { theme } from '../../styles/theme';
+import { styles } from "./styles";
+import { theme } from "../../styles/theme";
+
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
 
 export function Header() {
+  const opacity = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+    };
+  });
+
+  useEffect(() => {
+    opacity.value = withTiming(1, { duration: 3000 });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Feather
-        name="arrow-left"
-        color={theme.colors.brown}
-        size={32}
-      />
+    <Animated.View style={[styles.container, animatedStyle]}>
+      <Feather name="arrow-left" color={theme.colors.brown} size={32} />
 
       <View>
-        <Text style={styles.title}>
-          Oriental Food
-        </Text>
+        <Text style={styles.title}>Oriental Food</Text>
 
-        <Text style={styles.subtitle}>
-          Special Sushi
-        </Text>
+        <Text style={styles.subtitle}>Special Sushi</Text>
       </View>
-    </View>
+    </Animated.View>
   );
 }
