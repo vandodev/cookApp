36874ff932c0d06d73813285862d6f312c0c0d72
Evandro oliveira <vandoaparecido@hotmail.com>
2021-09-06 00:11:50 -0300

    :pizza: Animando prato com Moti

--- src/components/Plate/index.tsx
+++ src/components/Plate/index.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React from "react";
 
-import { styles } from './styles';
-import plateImg from '../../assets/plate.png';
+import { styles } from "./styles";
+import plateImg from "../../assets/plate.png";
+
+import { MotiImage } from "moti";
 
 export function Plate() {
   return (
-    <Image
+    <MotiImage
       style={styles.plate}
       source={plateImg}
       resizeMode="contain"
+      from={{
+        rotate: "100deg",
+        opacity: 0,
+      }}
+      animate={{
+        rotate: "0deg",
+        opacity: 1,
+      }}
+      transition={{
+        type: "timing",
+        duration: 2000,
+        // repeat: 3,
+        //loop:true tire a propriedade repeat
+        // repeatReverse: false,
+      }}
     />
   );
 }
